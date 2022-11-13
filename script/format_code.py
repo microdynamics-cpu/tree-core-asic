60109ba93602426e92fed7292bf3f7f01df62f50
maksyuki <maksyuki@126.com>
2022-11-13 21:10:26 +0800

    chore: modify prj code format

--- script/format_code.py
+++ script/format_code.py
@@ -1,10 +1,11 @@
 #!/bin/python
 
+import sys
 import os
 
 format_option = ''
 with open('.verible-format', 'r', encoding='utf-8') as fp:
     for line in fp:
         format_option += line
-os.system('verible-verilog-format ' + format_option + ' tb/core_tb.v')
-# print('verible-verilog-format ' + format_option)
+os.system('verible-verilog-format ' + format_option + ' ' + sys.argv[1])
+# print(sys.argv)
--- script/gen_soc.py
+++ script/gen_soc.py
@@ -1,7 +1,14 @@
 #!/bin/python
+
+import os
 import tomli
 
 with open('soc_config.toml', 'rb') as f:
     res = tomli.load(f)
 
 print(res)
+
+# print(os.getcwd())
+os.chdir('script/')
+os.system('./gen_wire.py')
+os.chdir('..')
