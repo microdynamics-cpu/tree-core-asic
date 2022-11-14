13765b1e31141c09976e200c5c399bec16ec84b8
maksyuki <maksyuki@126.com>
2022-11-14 09:42:18 +0800

    feat: add wire gen script and dat

--- script/gen_wire.py
+++ script/gen_wire.py
@@ -8,7 +8,7 @@ except Exception as e:
     print(e.args)
 
 newContent = []
-with open('./top_wire.dat', 'r', encoding='utf-8') as fp:
+with open('wire_dat/top.dat', 'r', encoding='utf-8') as fp:
     for line in fp:
         newContent.append(line)
     newContent.append('\n')
@@ -21,7 +21,7 @@ for v in cores:
         newContent.append('// ' + v + '\n')
         tmp = v.replace('ysyx_', '').replace('.v', '').replace('.sv', '')
         core_id.append(tmp)
-        with open('./io.dat', 'r', encoding='utf-8') as fp:
+        with open('wire_dat/io.dat', 'r', encoding='utf-8') as fp:
             for line in fp:
                 newContent.append(line.replace('xxxxxx', tmp))
 
@@ -101,38 +101,8 @@ for port in axi4_io:
                         cnt += 1
                 cnt = 0
 
-# newContent.append('\n//r\n')
-# for r_name in axi4_r:
-#     if r_name[0] != 'rready':
-#         for cid in core_id:
-#             newContent.append('assign io_master_' + r_name[0] + '_' + cid +
-#                               ' = core_dip == `ysyx_' + cid + ' ? ' +
-#                               r_name[0] + '_master_0 :' + r_name[1] +
-#                               '\'b0;\n')
-#         newContent.append('\n')
-#     else:
-#         for cid in core_id:
-#             if cnt == 0:
-#                 newContent.append('assign ' + r_name[0] +
-#                                   '_master_0   =  core_dip == `ysyx_' + cid +
-#                                   ' ? io_master_' + r_name[0] + '_' + cid +
-#                                   ' :\n')
-#                 cnt += 1
-#             elif cnt == len(core_id) - 1:
-#                 newContent.append(
-#                     '                          core_dip == `ysyx_' + cid +
-#                     ' ? io_master_' + r_name[0] + '_' + cid + ' : ' +
-#                     r_name[1] + '\'b0;\n\n')
-#                 cnt += 1
-#             else:
-#                 newContent.append(
-#                     '                          core_dip == `ysyx_' + cid +
-#                     ' ? io_master_' + r_name[0] + '_' + cid + ' :\n')
-#                 cnt += 1
-#         cnt = 0
-
 number = ['0', '1', '2', '3', '4', '5', '6', '7']
-newContent.append('\n//sram\n')
+newContent.append('\n// sram\n')
 cnt = 0
 for idx in number:
     for r_name in sram:
@@ -165,170 +135,75 @@ for idx in number:
                     cnt += 1
             cnt = 0
 
-newContent.append('//-------------------\n')
-newContent.append('//DMA signal\n\n')
-newContent.append('//aw\n')
-
-cnt = 0
-for aw_name in axi4_aw[1:]:
-    if aw_name[0] != 'awready':
-        for cid in core_id:
-            newContent.append('assign io_slave_' + aw_name[0] + '_' + cid +
-                              ' = core_dip == `ysyx_' + cid + ' ? ' +
-                              aw_name[0] + '_frontend :' + aw_name[1] +
-                              '\'b0;\n')
-        newContent.append('\n')
-    else:
-        for cid in core_id:
-            if cnt == 0:
-                newContent.append('assign ' + aw_name[0] +
-                                  '_frontend   =  core_dip == `ysyx_' + cid +
-                                  ' ? io_slave_' + aw_name[0] + '_' + cid +
-                                  ' :\n')
-                cnt += 1
-            elif cnt == len(core_id) - 1:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + aw_name[0] + '_' + cid + ' : ' +
-                    aw_name[1] + '\'b0;\n\n')
-                cnt += 1
+newContent.append('// DMA signal\n')
+for port in axi4_io:
+    newContent.append('// ' + port[0][0] + '\n')
+    cnt = 0
+    for v in port[1:]:
+        if 'ready' in v[0]:
+            if v[0] == 'bready' or v[0] == 'rready':
+                for cid in core_id:
+                    newContent.append('assign io_slave_' + v[0] + '_' + cid +
+                                      ' = core_dip == `ysyx_' + cid + ' ? ' +
+                                      v[0] + '_frontend :' + v[1] + '\'b0;\n')
+                newContent.append('\n')
             else:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + aw_name[0] + '_' + cid + ' :\n')
-                cnt += 1
-        cnt = 0
-
-newContent.append('\n//w\n')
-for w_name in axi4_w[1:]:
-    if w_name[0] != 'wready':
-        for cid in core_id:
-            newContent.append('assign io_slave_' + w_name[0] + '_' + cid +
-                              ' = core_dip == `ysyx_' + cid + ' ? ' +
-                              w_name[0] + '_frontend :' + w_name[1] +
-                              '\'b0;\n')
-        newContent.append('\n')
-    else:
-        for cid in core_id:
-            if cnt == 0:
-                newContent.append('assign ' + w_name[0] +
-                                  '_frontend   =  core_dip == `ysyx_' + cid +
-                                  ' ? io_slave_' + w_name[0] + '_' + cid +
-                                  ' :\n')
-                cnt += 1
-            elif cnt == len(core_id) - 1:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + w_name[0] + '_' + cid + ' : ' +
-                    w_name[1] + '\'b0;\n\n')
-                cnt += 1
+                for cid in core_id:
+                    if cnt == 0:
+                        newContent.append('assign ' + v[0] +
+                                          '_frontend   =  core_dip == `ysyx_' +
+                                          cid + ' ? io_slave_' + v[0] + '_' +
+                                          cid + ' :\n')
+                        cnt += 1
+                    elif cnt == len(core_id) - 1:
+                        newContent.append(
+                            '                          core_dip == `ysyx_' +
+                            cid + ' ? io_slave_' + v[0] + '_' + cid + ' : ' +
+                            v[1] + '\'b0;\n\n')
+                        cnt += 1
+                    else:
+                        newContent.append(
+                            '                          core_dip == `ysyx_' +
+                            cid + ' ? io_slave_' + v[0] + '_' + cid + ' :\n')
+                        cnt += 1
+        else:
+            if port[0][0] == 'b' or port[0][0] == 'r':
+                for cid in core_id:
+                    if cnt == 0:
+                        newContent.append('assign ' + v[0] +
+                                          '_frontend   =  core_dip == `ysyx_' +
+                                          cid + ' ? io_slave_' + v[0] + '_' +
+                                          cid + ' :\n')
+                        cnt += 1
+                    elif cnt == len(core_id) - 1:
+                        newContent.append(
+                            '                          core_dip == `ysyx_' +
+                            cid + ' ? io_slave_' + v[0] + '_' + cid + ' : ' +
+                            v[1] + "'b0;\n\n")
+                        cnt += 1
+                    else:
+                        newContent.append(
+                            '                          core_dip == `ysyx_' +
+                            cid + ' ? io_slave_' + v[0] + '_' + cid + ' :\n')
+                        cnt += 1
+                cnt = 0
             else:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + w_name[0] + '_' + cid + ' :\n')
-                cnt += 1
-        cnt = 0
-
-newContent.append('\n//b\n')
-for b_name in axi4_b[1:]:
-    if b_name[0] == 'bready':
-        for cid in core_id:
-            newContent.append('assign io_slave_' + b_name[0] + '_' + cid +
-                              ' = core_dip == `ysyx_' + cid + ' ? ' +
-                              b_name[0] + '_frontend :' + b_name[1] +
-                              '\'b0;\n')
-        newContent.append('\n')
-    else:
-        for cid in core_id:
-            if cnt == 0:
-                newContent.append('assign ' + b_name[0] +
-                                  '_frontend   =  core_dip == `ysyx_' + cid +
-                                  ' ? io_slave_' + b_name[0] + '_' + cid +
-                                  ' :\n')
-                cnt += 1
-            elif cnt == len(core_id) - 1:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + b_name[0] + '_' + cid + ' : ' +
-                    b_name[1] + "'b0;\n\n")
-                cnt += 1
-            else:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + b_name[0] + '_' + cid + ' :\n')
-                cnt += 1
-        cnt = 0
-
-newContent.append('\n//ar\n')
-for ar_name in axi4_ar[1:]:
-    if ar_name[0] != 'arready':
-        for cid in core_id:
-            newContent.append('assign io_slave_' + ar_name[0] + '_' + cid +
-                              ' = core_dip == `ysyx_' + cid + ' ? ' +
-                              ar_name[0] + '_frontend :' + ar_name[1] +
-                              '\'b0;\n')
-        newContent.append('\n')
-    else:
-        for cid in core_id:
-            if cnt == 0:
-                newContent.append('assign ' + ar_name[0] +
-                                  '_frontend   =  core_dip == `ysyx_' + cid +
-                                  ' ? io_slave_' + ar_name[0] + '_' + cid +
-                                  ' :\n')
-                cnt += 1
-            elif cnt == len(core_id) - 1:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + ar_name[0] + '_' + cid + ' : ' +
-                    ar_name[1] + '\'b0;\n\n')
-                cnt += 1
-            else:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + ar_name[0] + '_' + cid + ' :\n')
-                cnt += 1
-        cnt = 0
-
-newContent.append('\n//r\n')
-for r_name in axi4_r[1:]:
-    if r_name[0] == 'rready':
-        for cid in core_id:
-            newContent.append('assign io_slave_' + r_name[0] + '_' + cid +
-                              ' = core_dip == `ysyx_' + cid + " ? " +
-                              r_name[0] + "_frontend :" + r_name[1] + "'b0;\n")
-        newContent.append('\n')
-    else:
-        for cid in core_id:
-            if cnt == 0:
-                newContent.append('assign ' + r_name[0] +
-                                  '_frontend   =  core_dip == `ysyx_' + cid +
-                                  ' ? io_slave_' + r_name[0] + '_' + cid +
-                                  ' :\n')
-                cnt += 1
-            elif cnt == len(core_id) - 1:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + r_name[0] + '_' + cid + ' : ' +
-                    r_name[1] + '\'b0;\n\n')
-                cnt += 1
-            else:
-                newContent.append(
-                    '                          core_dip == `ysyx_' + cid +
-                    ' ? io_slave_' + r_name[0] + '_' + cid + ' :\n')
-                cnt += 1
-        cnt = 0
+                for cid in core_id:
+                    newContent.append('assign io_slave_' + v[0] + '_' + cid +
+                                      ' = core_dip == `ysyx_' + cid + ' ? ' +
+                                      v[0] + '_frontend :' + v[1] + '\'b0;\n')
+                newContent.append('\n')
 
 newContent.append('\n')
-newContent.append('//-------------------------------------\n')
-newContent.append('///cpu core\n')
+newContent.append('// cpu core\n')
 for cid in core_id:
-    with open('./3.txt', 'r', encoding='utf-8') as fp:
+    with open('wire_dat/core.dat', 'r', encoding='utf-8') as fp:
         for line in fp:
-            newContent.append(line.replace('000000', cid))
+            newContent.append(line.replace('xxxxxx', cid))
         newContent.append('\n')
 
 newContent.append('\n\n')
-with open('./4.txt', 'r', encoding='utf-8') as fp:
+with open('wire_dat/perip.dat', 'r', encoding='utf-8') as fp:
     for line in fp:
         newContent.append(line)
 
@@ -337,8 +212,8 @@ with open('../top/soc_top.v', 'w', encoding='utf-8') as fp:
 
 try:
     os.system("rm -f ../filelist/core.f")
-except:
-    pass
+except Exception as e:
+    print(e.args)
 
 newContent = []
 for root, dirs, files in os.walk('../cpu'):
