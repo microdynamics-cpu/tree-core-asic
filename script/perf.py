d257a977ce0f5b0d924956f0485210e6cd50bd2d
maksyuki <maksyuki@126.com>
2022-11-14 18:46:01 +0800

    chore: add color into pref script and format Makefile

--- script/perf.py
+++ script/perf.py
@@ -4,12 +4,15 @@ import sys
 import os
 import datetime
 
+CYELL = '\033[93m'
+CEND = '\033[0m'
+
 if sys.argv[1] == '-f':
     os.system('date +%s > perf.log')
     with open('perf.log', encoding='utf-8') as fp:
         start_time = fp.readline()
         res = os.popen('date +%T -d @' + start_time[:-1]).read()
-        print('SIMU START TIME: ' + res[:-1])
+        print(CYELL + '[Profiler] SIMU START TIME: ' + res[:-1] + CEND)
 elif sys.argv[1] == '-s':
     with open('perf.log', encoding='utf-8') as fp:
         start_time = fp.readline()
@@ -17,6 +20,6 @@ elif sys.argv[1] == '-s':
         res = os.popen('date +%T -d @' + end_time[:-1]).read()
         val1 = int(start_time)
         val2 = int(end_time)
-        print('SIMU END TIME: ' + res[:-1])
-        print('SIMU RUN TIME: ' +
-              str(datetime.timedelta(seconds=(val2 - val1))))
+        print(CYELL + '[Profiler] SIMU END TIME: ' + res[:-1] + CEND)
+        print(CYELL + '[Profiler] SIMU RUN TIME: ' +
+              str(datetime.timedelta(seconds=(val2 - val1))) + CEND)
