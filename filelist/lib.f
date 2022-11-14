1ec77e07244892faaeea092bb72d82e90a686256
maksyuki <maksyuki@126.com>
2022-11-14 19:26:25 +0800

    feat: add simple module for the ram

--- filelist/lib.f
+++
@@ -1,2 +0,0 @@
-../lib/asic/ram/S011HD1P_X32Y2D128.v
-../lib/asic/ram/S011HD1P_X32Y2D128_BW.v
--- ram/S011HD1P_X32Y2D128_BW.v
+++ ram/S011HD1P_X32Y2D128_BW.v
@@ -1,30 +1,22 @@
 module S011HD1P_X32Y2D128_BW (
-    Q,
-    CLK,
-    CEN,
-    WEN,
-    BWEN,
-    A,
-    D
+    output reg [     Bits-1:0] Q,
+    input                      CLK,
+    input                      CEN,
+    input                      WEN,
+    input      [Wen_Width-1:0] BWEN,
+    input      [Add_Width-1:0] A,
+    input      [     Bits-1:0] D
 );
-  parameter Bits = 128;
-  parameter Word_Depth = 64;
-  parameter Add_Width = 6;
-  parameter Wen_Width = 128;
-
-  output reg [Bits-1:0] Q;
-  input CLK;
-  input CEN;
-  input WEN;
-  input [Wen_Width-1:0] BWEN;
-  input [Add_Width-1:0] A;
-  input [Bits-1:0] D;
+  localparam Bits = 128;
+  localparam Word_Depth = 64;
+  localparam Add_Width = 6;
+  localparam Wen_Width = 128;
 
   wire cen = ~CEN;
   wire wen = ~WEN;
   wire [Wen_Width-1:0] bwen = ~BWEN;
+  reg  [     Bits-1:0] ram[0:Word_Depth-1];
 
-  reg [Bits-1:0] ram[0:Word_Depth-1];
   always @(posedge CLK) begin
     if (cen && wen) begin
       ram[A] <= (D & bwen) | (ram[A] & ~bwen);
